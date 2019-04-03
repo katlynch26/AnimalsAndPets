a220d7198efb9e6eb2959d8445e53b213609869c
lyu20 <32178113+lyu20@users.noreply.github.com>
2019-04-03 08:35:25 -0400

    Add files via upload

---
+++ RainbowSloth.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class RainbowSloth here.
+ *
+ * @author Linda
+ * @version 2019
+ */
+public class RainbowSloth extends Animal
+{
+    
+    public RainbowSloth()
+    {
+        super();
+        setSpecies("Rainbow Sloth");
+    }
+    
+    public void eat()
+    {
+        System.out.println("Yum! Froot Loops! Also leaves, buds, twigs, fruits and occasionally, insects and rodents! \t");
+    }
+    
+    public void speak()
+    {
+        System.out.print(" < mehh > ");
+    }
+}
